65ce523e74be7c7741c3f5fefea9c2869074c4a8
nader <nader.davarmanesh62@gmail.com>
2023-11-27 10:33:52 +0330

    lesson 62 :make featured product in homepage

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -15,7 +15,8 @@ class IndexController extends Controller
         $newProducts=Product::orderBY('id','Desc')->where('status',1)->limit(4)->get()->toArray();
         $bestSeller=Product::where(['is_bestseller'=>'Yes','status'=>1])->inRandomOrder()->get()->toArray();
         $discountedProduct=Product::where('product_discount','>',0)->where('status',1)->limit(6)->inRandomOrder()->get()->toArray();
+        $featuredProduct=Product::where(['is_featured'=>'Yes','status'=>1])->limit(6)->get()->toArray();
 
-        return view('front.index')->with(compact('sliderBanner','fixBanner','newProducts','bestSeller','discountedProduct'));
+        return view('front.index')->with(compact('sliderBanner','fixBanner','newProducts','bestSeller','discountedProduct','featuredProduct'));
     }
 }
