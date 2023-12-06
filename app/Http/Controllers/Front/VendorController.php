fdfc6cfb11707f059f5c2c8c0178e36155e6bd7c
nader <nader.davarmanesh62@gmail.com>
2023-12-06 09:22:12 +0330

    lesson 81:make vendor login and registeration form in this part make vendor controller and blade

---
+++ app/Http/Controllers/Front/VendorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VendorController extends Controller
+{
+    public function LoginRegister(){
+        return view('front.vendors.login_register');
+
+    }
+}
